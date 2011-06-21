ef210ce667db27f6ee4db8713c213bc36d234ca8
Kevin McKelvin <kmckelvin@gmail.com>
2011-06-21 02:09:31 +0200

    Created basic tests for HookedDbContext

---
+++ EFHooks.Tests/HookedDbContextTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using EFHooks.Tests.Hooks;
+using NUnit.Framework;
+
+namespace EFHooks.Tests
+{
+    public class HookedDbContextTests
+    {
+        private class TimestampPreInsertHook : PreInsertHook<ITimeStamped>
+        {
+            public override void Hook(ITimeStamped entity, HookEntityMetadata metadata)
+            {
+                entity.CreatedAt = DateTime.Now;
+            }
+        }
+
+        private class LocalContext : HookedDbContext
+        {
+            public LocalContext(IEnumerable<IHook> hooks) : base(hooks)
+            {
+                
+            }
+
+            public DbSet<TimestampedSoftDeletedEntity> Entities { get; set; }
+        }
+
+        [Test]
+        public void HookedDbContext_ConstructsWithHooks()
+        {
+            var hooks = new IHook[]
+                            {
+                                new TimestampPreInsertHook()
+                            };
+
+            var context = new LocalContext(hooks);
+        }
+
+        [Test]
+        public void HookedDbContext_MustNotCallHooks_WhenGetValidationErrorsIsCalled()
+        {
+            var hooks = new IHook[]
+                            {
+                                new TimestampPreInsertHook()
+                            };
+
+            var context = new LocalContext(hooks);
+            var entity = new TimestampedSoftDeletedEntity();
+            context.Entities.Add(entity);
+            context.GetValidationErrors();
+
+            Assert.AreNotEqual(entity.CreatedAt.Date, DateTime.Today);
+        }
+
+        [Test]
+        public void HookedDbContext_MustCallHooks_WhenRunningSaveChanges()
+        {
+            var hooks = new IHook[]
+                            {
+                                new TimestampPreInsertHook()
+                            };
+
+            var context = new LocalContext(hooks);
+            var entity = new TimestampedSoftDeletedEntity();
+            context.Entities.Add(entity);
+            context.SaveChanges();
+
+            Assert.AreEqual(entity.CreatedAt.Date, DateTime.Today);
+        }
+    }
+}
--- EFHooks/HookedDbContext.cs
+++ EFHooks/HookedDbContext.cs
@@ -1,9 +1,15 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace EFHooks
 {
     public class HookedDbContext : DbContext
     {
+        public HookedDbContext(IEnumerable<IHook> hooks)
+        {
+            
+        }
+
         protected override System.Data.Entity.Validation.DbEntityValidationResult ValidateEntity(System.Data.Entity.Infrastructure.DbEntityEntry entityEntry, System.Collections.Generic.IDictionary<object, object> items)
         {
             return base.ValidateEntity(entityEntry, items);

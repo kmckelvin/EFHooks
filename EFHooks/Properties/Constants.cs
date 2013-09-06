18f3a83ba572183ec8bde25d5ceb9d2dd7148c9f
Robert Sirre <info@robertsirre.nl>
2013-09-06 17:44:47 +0200

    Used centralized constant for version number, bumped version

---
+++ EFHooks/Properties/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace EFHooks.Properties
+{
+	/// <summary>
+	/// EFHooks constant values
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>
+		/// The version
+		/// </summary>
+		public const string Version = "1.0.5.4";
+	}
+
+}

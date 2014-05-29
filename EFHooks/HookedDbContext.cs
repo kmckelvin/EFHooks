37f277e562bb20ed6821bffded4c3a826bf76754
George Mauer <gmauer@gmail.com>
2014-05-29 00:03:25 -0200

    Async tests are passing.

--- EFHooks/HookedDbContext.cs
+++ EFHooks/HookedDbContext.cs
@@ -2,6 +2,8 @@
 using System.Data.Entity;
 using System.Data.Common;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EFHooks
 {
@@ -113,72 +115,91 @@ namespace EFHooks
 		/// <returns>
 		/// The number of objects written to the underlying database.
 		/// </returns>
-		public override int SaveChanges()
-		{
-			var modifiedEntries = this.ChangeTracker.Entries()
-											.Where(x => x.State != EntityState.Unchanged && x.State != EntityState.Detached)
-											.Select(x => new HookedEntityEntry()
-											{
-												Entity = x.Entity,
-												PreSaveState = x.State
-											})
-											.ToArray();
+		public override int SaveChanges() {
 
-			ExecutePreActionHooks(modifiedEntries, false);//Regardless of validation (executing the hook possibly fixes validation errors)
-
-			var hasValidationErrors = this.Configuration.ValidateOnSaveEnabled && this.ChangeTracker.Entries().Any(x => x.State != EntityState.Unchanged && !x.GetValidationResult().IsValid);
-			var hasPostHooks = this.PostHooks.Any(); // Save this to a local variable since we're checking this again later.
-
-			if (!hasValidationErrors)
-			{
-				ExecutePreActionHooks(modifiedEntries, true);
-			}
-
+		    var hookExecution = new HookRunner(this);
+            hookExecution.RunPreActionHooks();
 			var result = base.SaveChanges();
-
-			if (hasPostHooks)
-			{
-				foreach (var entityEntry in modifiedEntries)
-				{
-					var entry = entityEntry;
-
-					//Obtains hooks that 'listen' to one or more Entity States
-					foreach (var hook in PostHooks.Where(x => (x.HookStates & entry.PreSaveState) == entry.PreSaveState))
-					{
-						var metadata = new HookEntityMetadata(entityEntry.PreSaveState, this);
-						hook.HookObject(entityEntry.Entity, metadata);
-					}
-				}
-			}
-
+		    hookExecution.RunPostActionHooks();
 			return result;
 		}
-        
-		/// <summary>
-		/// Executes the pre action hooks, filtered by <paramref name="requiresValidation"/>.
-		/// </summary>
-		/// <param name="modifiedEntries">The modified entries to execute hooks for.</param>
-		/// <param name="requiresValidation">if set to <c>true</c> executes hooks that require validation, otherwise executes hooks that do NOT require validation.</param>
-		private void ExecutePreActionHooks(IEnumerable<HookedEntityEntry> modifiedEntries, bool requiresValidation)
-		{
-			foreach (var entityEntry in modifiedEntries)
-			{
-				var entry = entityEntry; //Prevents access to modified closure
 
-				foreach (
-					var hook in
-						PreHooks.Where(x => (x.HookStates & entry.PreSaveState) == entry.PreSaveState
-											&& x.RequiresValidation == requiresValidation))
-				{
-					var metadata = new HookEntityMetadata(entityEntry.PreSaveState, this);
-					hook.HookObject(entityEntry.Entity, metadata);
+	    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken) {
+            var hookExecution = new HookRunner(this);
+            hookExecution.RunPreActionHooks();
+            var result = await base.SaveChangesAsync(cancellationToken);
+            hookExecution.RunPostActionHooks();
+            return result;
+        }
 
-					if (metadata.HasStateChanged)
-					{
-						entityEntry.PreSaveState = metadata.State;
-					}
-				}
-			}
-		}
-	}
+	    class HookRunner
+	    {
+	        private HookedDbContext ctx;
+            private HookedEntityEntry[] modifiedEntries;
+
+	        public HookRunner(HookedDbContext ctx) {
+	            this.ctx = ctx;
+                this.modifiedEntries = ctx.ChangeTracker.Entries()
+                                                .Where(x => x.State != EntityState.Unchanged && x.State != EntityState.Detached)
+                                                .Select(x => new HookedEntityEntry() {
+                                                    Entity = x.Entity,
+                                                    PreSaveState = x.State
+                                                })
+                                                .ToArray();
+
+	        }
+
+	        public void RunPreActionHooks() {
+                ExecutePreActionHooks(modifiedEntries, false);//Regardless of validation (executing the hook possibly fixes validation errors)
+
+                var hasValidationErrors = ctx.Configuration.ValidateOnSaveEnabled && ctx.ChangeTracker.Entries().Any(x => x.State != EntityState.Unchanged && !x.GetValidationResult().IsValid);
+
+                if (!hasValidationErrors) {
+                    ExecutePreActionHooks(modifiedEntries, true);
+                }
+	            
+	        }
+
+
+	        /// <summary>
+		    /// Executes the pre action hooks, filtered by <paramref name="requiresValidation"/>.
+		    /// </summary>
+		    /// <param name="modifiedEntries">The modified entries to execute hooks for.</param>
+		    /// <param name="requiresValidation">if set to <c>true</c> executes hooks that require validation, otherwise executes hooks that do NOT require validation.</param>
+		    private void ExecutePreActionHooks(IEnumerable<HookedEntityEntry> modifiedEntries, bool requiresValidation)
+		    {
+			    foreach (var entityEntry in modifiedEntries)
+			    {
+				    var entry = entityEntry; //Prevents access to modified closure
+
+				    foreach (var hook in ctx.PreHooks.Where(x => (x.HookStates & entry.PreSaveState) == entry.PreSaveState && x.RequiresValidation == requiresValidation))
+				    {
+					    var metadata = new HookEntityMetadata(entityEntry.PreSaveState, ctx);
+					    hook.HookObject(entityEntry.Entity, metadata);
+
+					    if (metadata.HasStateChanged)
+					    {
+						    entityEntry.PreSaveState = metadata.State;
+					    }
+				    }
+			    }
+		    }
+
+	        public void RunPostActionHooks() {
+                var hasPostHooks = ctx.PostHooks.Any(); // Save this to a local variable since we're checking this again later.
+                if (hasPostHooks) {
+                    foreach (var entityEntry in modifiedEntries) {
+                        var entry = entityEntry;
+
+                        //Obtains hooks that 'listen' to one or more Entity States
+                        foreach (var hook in ctx.PostHooks.Where(x => (x.HookStates & entry.PreSaveState) == entry.PreSaveState)) {
+                            var metadata = new HookEntityMetadata(entityEntry.PreSaveState, ctx);
+                            hook.HookObject(entityEntry.Entity, metadata);
+                        }
+                    }
+                }
+
+	        }
+	    }
+    }
 }
